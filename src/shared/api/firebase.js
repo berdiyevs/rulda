76bd3f015132e4c4f776612800556661701713a6
Sarvarbek Berdiyev <berdiyevs2006@gmail.com>
2026-08-22 10:17:32 +0500

    .env.exmple added

--- src/shared/api/firebase.js
+++ src/shared/api/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDgaqZvg85uLW5TxUcOWGfXyFgzx6-LI5E',
-  authDomain: 'pravaquiz.firebaseapp.com',
-  projectId: 'pravaquiz',
-  storageBucket: 'pravaquiz.firebasestorage.app',
-  messagingSenderId: '840733117812',
-  appId: '1:840733117812:web:f0507b70ed517213853021',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
